ba67236efd452683c452926fdb38938cee4cfe31
dsaikiran01 <dsaikiran559@gmail.com>
2025-12-28 06:00:31 +0530

    added health test endpoint

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,11 @@ app.use("/api/auth", authRoutes);
 // DB + Server
 connectDB();
 
+// health test endpoint
+app.get('/health', (req, res) => {
+  res.status(200).json({ message: 'Service is up and running!' });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
